281d33cf340ac454cd5c1b1d5b20beefda9b4aca
Atlas16A <thegoldenatlaspass@gmail.com>
2024-11-09 19:40:25 -0500

    node graph idea

---
+++ bevy_editor_panes/bevy_node_graph/src/lib.rs
@@ -0,0 +1,9 @@
+//! A Node Graph Editor for Bevy.
+use bevy::prelude::*;
+
+/// The Bevy Marketplace Viewer Plugin.
+pub struct NodeGraphPlugin;
+
+impl Plugin for NodeGraphPlugin {
+    fn build(&self, _app: &mut App) {}
+}
